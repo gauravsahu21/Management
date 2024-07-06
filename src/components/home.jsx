fe05e0527bd733b99d320d723036d7a5f0806e3e
Gaurav Kumar <gk989kumar@gmail.com>
2024-07-06 21:10:53 +0530

    task details

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,25 +1,37 @@
-import { SignedIn, SignedOut, SignIn, UserButton,useUser  } from "@clerk/clerk-react";
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  UserButton,
+  useUser,
+} from "@clerk/clerk-react";
+import Task from "./task";
 
 const Home = () => {
-    const { isSignedIn, user, isLoaded } = useUser();
-  
-  console.log(user?.emailAddresses[0].emailAddress||"dsaf");
-  return <>
-  <header>
-  <SignedIn>
-      {/* <div>Hello {user?.fullName||"full"}!</div>
+  const { isSignedIn, user, isLoaded } = useUser();
+
+  console.log(user?.emailAddresses[0].emailAddress || "dsaf");
+  return (
+    <>
+      <header>
+        <SignedIn>
+          {/* <div>Hello {user?.fullName||"full"}!</div>
       <div>Hello {user?.firstName||"first"}!</div>
       <div>Hello {user?.firstName||"first"}!</div> */}
-     <div className="flex justify-end m-2 bg-blue-300 p-2 rounded-md">
-     <UserButton showName />
-     </div>
-     <div className="">
-
-     </div>
-        
-      </SignedIn>
-  </header>
-  </>;
+          <div className="flex justify-end m-2 bg-blue-300 p-2 rounded-md">
+            <UserButton showName />
+          </div>
+         <div className="flex justify-end ">
+            <div className="bg-blue-500 p-2 rounded-md m-2">+ Add Task</div>       
+         </div>
+          <Task/>
+          <Task/>
+          <Task/>
+          <Task/>
+        </SignedIn>
+      </header>
+    </>
+  );
 };
 
 export default Home;
---
+++ src/components/task.jsx
@@ -0,0 +1,19 @@
+const Task = () => {
+  return (
+    <div className="mt-6 min-h-36 bg-blue-200 w-[80%] rounded-md mx-auto p-4">
+      <div className="flex justify-between">
+        <p>Task Name: Task 1</p>
+        <p className=" bg-red-500 p-1 w-16 rounded-md">Priority</p>
+      </div>
+      <p>
+        Description : dsaaaaaaaaaaaaaavasdvas dasvvvvvvvvvvvvv davsdasd
+        vdasddddddddd dvasd
+      </p>
+      <p>Due Date:</p>
+
+      <p>Status: 1</p>
+    </div>
+  );
+};
+
+export default Task;
