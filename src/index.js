8f43544a4c73f5e82b1f9e00525fe1612c2d4aea
Gaurav Kumar <gk989kumar@gmail.com>
2024-07-06 10:05:24 +0530

    formating

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ClerkProvider } from '@clerk/clerk-react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-const PUBLISHABLE_KEY = "pk_test_cmVzb2x2ZWQtbW9yYXktMTIuY2xlcmsuYWNjb3VudHMuZGV2JA";
+const PUBLISHABLE_KEY =
+  "pk_test_cmVzb2x2ZWQtbW9yYXktMTIuY2xlcmsuYWNjb3VudHMuZGV2JA";
 if (!PUBLISHABLE_KEY) {
-  console.log("gaurav is great")
-  throw new Error("Missing Publishable Key")
+  console.log("gaurav is great");
+  throw new Error("Missing Publishable Key");
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App/>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
     </ClerkProvider>
   </React.StrictMode>
 );
