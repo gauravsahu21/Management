b9c1d97980dfab5db3f25af20a6fa1a85572b59d
Gaurav Kumar <gk989kumar@gmail.com>
2024-07-06 20:40:01 +0530

    task

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,25 @@
-
+import { SignedIn, SignedOut, SignIn, UserButton,useUser  } from "@clerk/clerk-react";
 
 const Home = () => {
-  return (
-    <div>home</div>
-  )
-}
+    const { isSignedIn, user, isLoaded } = useUser();
+  
+  console.log(user?.emailAddresses[0].emailAddress||"dsaf");
+  return <>
+  <header>
+  <SignedIn>
+      {/* <div>Hello {user?.fullName||"full"}!</div>
+      <div>Hello {user?.firstName||"first"}!</div>
+      <div>Hello {user?.firstName||"first"}!</div> */}
+     <div className="flex justify-end m-2 bg-blue-300 p-2 rounded-md">
+     <UserButton showName />
+     </div>
+     <div className="">
+
+     </div>
+        
+      </SignedIn>
+  </header>
+  </>;
+};
 
 export default Home;
